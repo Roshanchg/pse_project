4a1bdbbece997a92a1948fcc025f47e36b33c59c
Roshan Chg <chgroshan6@gmail.com>
2025-03-05 14:18:31 +0545

    'mixed' seciton and 'popular' section templates with styles done

--- tourism/website/models.py
+++ tourism/website/models.py
@@ -8,4 +8,11 @@ class Users(models.Model):
     def __str__(self):
         return f"{self.name} as {self.email}"
 
-    
+class Destinations(models.Model):
+    name=models.CharField(max_length=30,null=False)
+    imagename=models.CharField(max_length=50,unique=False,null=False)
+    times=models.CharField(max_length=20,unique=False,null=False)
+    price=models.IntegerField(null=False)
+    description=models.CharField(max_length=100,unique=False,null=True)
+    def __str__(self):
+        return f"{self.name} at {self.imagename}"
