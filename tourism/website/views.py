bb84131a9613244690724b7a4c5cf926a0d362f2
Roshan Chg <chgroshan6@gmail.com>
2025-03-06 16:53:51 +0545

    accounts page made, Front end template and style

--- tourism/website/views.py
+++ tourism/website/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 # Create your views here.
 def index(request):
-    return render(request=request,template_name="index.html",context=None)
+    return render(request=request,template_name="accountedit.html",context=None)
